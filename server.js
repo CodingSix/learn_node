a09e6b996943741afc219798573d196cdec959f9
codingsix <codingsix@gmail.com>
2017-07-03 18:31:21 +0800

    add express and some modules

--- server.js
+++ server.js
@@ -1,32 +1,16 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 var fs = require('fs');
-var url = require('url');
 
+app.get('/listUsers',function(req,res){
+	fs.readFile(__dirname + "/" + "users.json","utf-8",function(err,data){
+		console.log(data);
+		res.end(data);
+	})
+})
 
-// 创建服务器
-http.createServer( function (request, response) {  
-   // 解析请求，包括文件名
-   var pathname = url.parse(request.url).pathname;
-   
-   // 输出请求的文件名
-   console.log("Request for " + pathname + " received.");
-   
-   // 从文件系统中读取请求的文件内容
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP 状态码: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else{	         
-         // HTTP 状态码: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});	
-         
-         // 响应文件内容
-         response.write(data.toString());		
-      }
-      //  发送响应数据
-      response.end();
-   });   
-}).listen(8081);
+var server = app.listen(8081,function(){
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log("实例访问地址%s%s",host,port)
+})
